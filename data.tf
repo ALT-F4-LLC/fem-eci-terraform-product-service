a1cbe340986d17c75064ade33d94de30217960a4
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2023-09-24 21:54:50 -0700

    Added configurations (#3)
    * feat: added dynamic configuration
    
    * feat: fix data

---
+++ data.tf
@@ -0,0 +1,4 @@
+data "tfe_outputs" "tfe" {
+  organization = "<your-organization-name>"
+  workspace    = "fem-eci-tfe"
+}
--- main.tf
+++ main.tf
@@ -1,13 +1,15 @@
 module "service" {
+  for_each = local.services
+
   source = "./module/ecs-service"
 
   cluster_name = var.cluster_name
   environment  = var.environment
   name         = "service"
+  parameters   = each.value.parameters
+}
 
-  parameters = [
-    "fem-instructor",
-    "fem-location",
-    "fem-version"
-  ]
+moved {
+  from = module.service
+  to   = module.service["service"]
 }
